10421c59de303cb45e5da8754f2ae005f1a61a94
fleisan18 <92662450+fleisan18@users.noreply.github.com>
2022-09-02 17:02:59 +0500

    added HAVING

--- 02-Простые запросы с SELECT/GROUP BY.sql
+++ 02-Простые запросы с SELECT/GROUP BY.sql
@@ -9,3 +9,10 @@ FROM products
 GROUP BY category_id
 ORDER BY SUM (units_in_stock)
 LIMIT 5;
+
+SELECT category_id, SUM (unit_price * units_in_stock)
+FROM products
+WHERE discontinued != 1
+GROUP BY category_id
+HAVING SUM (unit_price * units_in_stock) > 5000
+ORDER BY SUM (unit_price * units_in_stock) DESC;
