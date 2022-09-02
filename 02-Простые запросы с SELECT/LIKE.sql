e369cc145d788b437175188a45d991877e5dfeb1
fleisan18 <92662450+fleisan18@users.noreply.github.com>
2022-09-02 16:46:28 +0500

    Create LIKE.sql

---
+++ 02-Простые запросы с SELECT/LIKE.sql
@@ -0,0 +1,14 @@
+-- % - сколько угодно символов
+-- - ровно 1 символ
+
+SELECT last_name, first_name
+FROM employees
+WHERE first_name LIKE '%n';
+
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE 'B%';
+
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE '_uch%';
