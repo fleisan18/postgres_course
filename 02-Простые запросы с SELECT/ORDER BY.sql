acf86e6cce74d9c17fdb957677cdd6ec99dff533
fleisan18 <92662450+fleisan18@users.noreply.github.com>
2022-09-02 16:07:57 +0500

    Create ORDER BY.sql

---
+++ 02-Простые запросы с SELECT/ORDER BY.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT country
+FROM customers
+ORDER BY country;
+
+SELECT DISTINCT country
+FROM customers
+ORDER BY country DESC;
+
+SELECT DISTINCT country, city
+FROM customers
+ORDER BY country DESC, city DESC;
