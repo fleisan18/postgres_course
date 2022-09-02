96eac7cc62962cf2d28e1741e36242686ca8bea8
fleisan18 <92662450+fleisan18@users.noreply.github.com>
2022-09-02 09:11:51 +0500

    Create BETWEEN.sql

---
+++ 02-Простые запросы с SELECT/BETWEEN.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*)
+FROM orders
+WHERE freight >= 20 AND freight <= 40;
+
+SELECT COUNT(*)
+FROM orders
+WHERE freight BETWEEN 20 AND 40; --including boundaries
+
+SELECT *
+FROM orders
+WHERE order_date BETWEEN '1998-03-30' AND '1998-04-30';
